38d06c32e5ccc2adfb5ddb8f9fa651d7a6f8734f
Maxim Frolov <frolov-max74@yandex.ru>
2016-01-29 01:12:52 +0500

    протестировал методы execute(), query()

--- tests/test.php
+++ tests/test.php
@@ -2,11 +2,23 @@
 
 require __DIR__ . '/../autoload.php';
 
-$userOne = new \App\Db();
-$userOne->query(
+/*$getUserOne = new \App\Db();
+$res = $getUserOne->query(
     'SELECT * FROM users WHERE id=:id',
     '\App\Models\User',
-    [':id' => 2]
+    [':id' => 1,]
 );
 
-var_dump($userOne);
+var_dump($res);
+
+$insertOneUser = new \App\Db();
+$res = $insertOneUser->execute(
+    'INSERT INTO users (email, name) VALUES (:email, :name)',
+    [
+        ':email' => 'leshukovaanna@gmail.com',
+        ':name'  => 'Анна',
+    ]
+);
+
+var_dump($res);*/
+
