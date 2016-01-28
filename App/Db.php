7e8b553f3e27f5d0987f7a939f870f2ddba00636
Maxim Frolov <frolov-max74@yandex.ru>
2016-01-28 12:26:13 +0500

    добавил метод execute(), протестировал

--- App/Db.php
+++ App/Db.php
@@ -2,11 +2,30 @@
 
 namespace App;
 
+use \PDO;
 
 class Db
 {
+    protected $dbh;
+
     public function __construct()
     {
-        echo 'HELLO Db!';
+        $config = include __DIR__ . '/../config.php';
+        $dsn = $config['driver'] . ':dbname=' . $config['dbname'] . ';host=' . $config['host'];
+        $this->dbh = new PDO(
+            $dsn,
+            $config['user'],
+            $config['password'],
+            [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',]
+        );
+        $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    }
+
+    public function execute($sql)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute();
+        return $res;
     }
 }
