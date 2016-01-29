b09e36b3f2afd5e155b54fd49b1524cce37e633d
Maxim Frolov <frolov-max74@yandex.ru>
2016-01-29 18:27:41 +0500

    + шаблон для вывода последних новостей

--- App/Views/lastNews.php
+++ App/Views/lastNews.php
@@ -1,2 +1,24 @@
-<?php
-
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Последние новости</title>
+</head>
+<body>
+    <ul>
+        <?php foreach($lastNews as $val): ?>
+            <li>
+                <header>
+                    <a href="/App/Controllers/article.php?id=<?php echo $val->id; ?>">
+                        <h2><?php echo $val->title; ?></h2>
+                    </a>
+                </header>
+                <article>
+                    <?php echo $val->text; ?>
+                </article>
+                <hr>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
