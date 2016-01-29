77e49a2b3e911d53fce334332a9c062a3892b1c8
Maxim Frolov <frolov-max74@yandex.ru>
2016-01-29 21:12:37 +0500

    - try catch

--- index.php
+++ index.php
@@ -4,13 +4,6 @@ use \App\Models\News;
 
 require __DIR__ . '/autoload.php';
 
-try {
+$lastNews = News::lastNews();
 
-    $lastNews = News::lastNews();
-
-} catch (PDOException $e) {
-
-    echo 'Подключение не удалось' . $e->getMessage();
-
-}
 include __DIR__ . '/App/Views/lastNews.php';
