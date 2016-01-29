3baf14e62e11f94046e95ee485458fe704d2b131
Maxim Frolov <frolov-max74@yandex.ru>
2016-01-29 16:06:23 +0500

    + тест вывода массива объектов новостей, подключил шаблон вывода

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 
+use \App\Models\News;
+
 require __DIR__ . '/autoload.php';
 
+try {
+
+    $lastNews = News::lastNews();
+    var_dump($lastNews);
+
+} catch (PDOException $e) {
+
+    echo 'Подключение не удалось' . $e->getMessage();
+
+}
+include __DIR__ . '/App/Views/lastNews.php';
