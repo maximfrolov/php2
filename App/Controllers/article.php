fb6087f6962a509a9212e2d4f0730a8c0f1760d8
Maxim Frolov <frolov-max74@yandex.ru>
2016-01-29 18:25:18 +0500

    + контроллер article.php

--- App/Controllers/article.php
+++ App/Controllers/article.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Maxim
- * Date: 29.01.2016
- * Time: 16:19
- */
+
+use \App\Models\News;
+
+require __DIR__ . '/../../autoload.php';
+
+try {
+
+    if(!empty($_GET['id'])) {
+        $id = $_GET['id'];
+        $article = News::findById($id);
+        include __DIR__ . '/../Views/oneNews.php';
+    } else {
+        header('Location: /');
+        exit;
+    }
+
+} catch (PDOException $e) {
+
+    echo 'Подключение не удалось' . $e->getMessage();
+}
