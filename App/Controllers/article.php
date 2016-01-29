8364f53c42908dafe51062f3947359d26fdec16f
Maxim Frolov <frolov-max74@yandex.ru>
2016-01-29 21:09:17 +0500

    - try catch из контроллера

--- App/Controllers/article.php
+++ App/Controllers/article.php
@@ -4,18 +4,11 @@ use \App\Models\News;
 
 require __DIR__ . '/../../autoload.php';
 
-try {
-
-    if(!empty($_GET['id'])) {
-        $id = $_GET['id'];
-        $article = News::findById($id);
-        include __DIR__ . '/../Views/oneNews.php';
-    } else {
-        header('Location: /');
-        exit;
-    }
-
-} catch (PDOException $e) {
-
-    echo 'Подключение не удалось' . $e->getMessage();
+if(!empty($_GET['id'])) {
+    $id = $_GET['id'];
+    $article = News::findById($id);
+    include __DIR__ . '/../Views/oneNews.php';
+} else {
+    header('Location: /');
+    exit;
 }
