fd56131c9fd20fddb803a3fc09afea4cc9385474
Maxim Frolov <frolov-max74@yandex.ru>
2016-01-31 17:38:50 +0500

    тест Singleton в классе Config

--- tests/test.php
+++ tests/test.php
@@ -2,23 +2,9 @@
 
 require __DIR__ . '/../autoload.php';
 
-/*$getUserOne = new \App\Db();
-$res = $getUserOne->query(
-    'SELECT * FROM users WHERE id=:id',
-    '\App\Models\User',
-    [':id' => 1,]
-);
+$config = \App\Config::instance();
+$config->counter = 1;
+var_dump($config);
 
-var_dump($res);
-
-$insertOneUser = new \App\Db();
-$res = $insertOneUser->execute(
-    'INSERT INTO users (email, name) VALUES (:email, :name)',
-    [
-        ':email' => 'leshukovaanna@gmail.com',
-        ':name'  => 'Анна',
-    ]
-);
-
-var_dump($res);*/
-
+$config = \App\Config::instance();
+var_dump($config);
