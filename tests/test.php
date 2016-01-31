a1f40264f53d817458a501c27531a23e4a5dea78
Maxim Frolov <frolov-max74@yandex.ru>
2016-01-31 19:21:51 +0500

    + тест класса Config

--- tests/test.php
+++ tests/test.php
@@ -2,9 +2,22 @@
 
 require __DIR__ . '/../autoload.php';
 
-$config = \App\Config::instance();
+/*$config = \App\Config::instance();
 $config->counter = 1;
 var_dump($config);
 
 $config = \App\Config::instance();
+var_dump($config);*/
+
+// тест класса Config
+/*$config = \App\Config::instance();
+$config->counter = 1;
+echo $config->data['db']['driver'];
+echo $config->data['db']['dbname'];
+echo $config->data['db']['host'];
+echo $config->data['db']['user'];
+echo $config->data['db']['password'];
 var_dump($config);
+
+$config = \App\Config::instance();
+var_dump($config);*/
