aaf0ed8c1cbcad23fa7bd215373ee65dc76b330d
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-02 15:39:49 +0500

    + контроллер добавления новости

---
+++ App/Controllers/add.php
@@ -0,0 +1,16 @@
+<?php
+
+use \App\Models\News;
+
+require __DIR__ . '/../../autoload.php';
+
+if (!empty($_POST['title']) && !empty($_POST['text'])) {
+    $article = new News();
+    $article->title = $_POST['title'];
+    $article->text = $_POST['text'];
+    $article->save();
+    header('Location: /');
+    exit;
+} else {
+    include __DIR__ . '/../Views/add.php';
+}
