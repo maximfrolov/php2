f2e18ed0bcee3796d296c0b61c22a0938d271180
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-02 15:42:59 +0500

    + контроллер удаления новости

---
+++ App/Controllers/delete.php
@@ -0,0 +1,15 @@
+<?php
+
+use \App\Models\News;
+
+require __DIR__ . '/../../autoload.php';
+
+if (!empty($_POST['id'])) {
+    $id = $_POST['id'];
+    $article = News::findById($id);
+    $article->delete();
+    header('Location: /');
+    exit;
+} else {
+    header('Location: /');
+}
