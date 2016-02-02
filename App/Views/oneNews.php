6c10ed907db6493c727868759c087ee11fc6bafb
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-02 15:29:19 +0500

    + форма редактирования/удаления новости

--- App/Views/oneNews.php
+++ App/Views/oneNews.php
@@ -2,7 +2,7 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Новость <?php echo $article->id ?></title>
+    <title><?php echo $article->title ?></title>
 </head>
 <body>
     <header>
@@ -11,5 +11,40 @@
     <article>
         <?php echo $article->text; ?>
     </article>
+    <hr>
+
+    <h2>Обновление новости:</h2>
+    <form method="post" action="/App/Controllers/update.php">
+        <label for="id">ID новости:</label>
+        <br>
+        <input type="number" name="id" value="<?php echo $article->id ?>">
+        <br>
+        <label for="title">Заголовок:</label>
+        <br>
+        <input type="text" name="title" value="<?php echo $article->title; ?>">
+        <br>
+        <label for="text">Текст:</label>
+        <br>
+        <textarea cols="50" rows="5" name="text"><?php echo $article->text; ?></textarea>
+        <br>
+        <p>
+            <button type="submit">Обновить</button>
+        </p>
+    </form>
+
+    <h2>Удаление новости:</h2>
+    <form method="post" action="/App/Controllers/delete.php">
+        <label for="id">ID новости:</label>
+        <br>
+        <input type="number" name="id" value="<?php echo $article->id ?>">
+        <p>
+            <button type="submit">Удалить</button>
+        </p>
+    </form>
+    <p>
+        <a href="/index.php">
+            <button>На главную</button>
+        </a>
+    </p>
 </body>
 </html>
