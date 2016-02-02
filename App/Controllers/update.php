3fa2769d51d6bf875c065c75249ecd00c38be8fb
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-02 15:41:14 +0500

    + контроллер обновления новости

---
+++ App/Controllers/update.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\News;
+
+require __DIR__ . '/../../autoload.php';
+
+if (!empty($_POST['title']) &&
+    !empty($_POST['text']) &&
+    !empty($_POST['id'])) {
+        $id = $_POST['id'];
+        $article = News::findById($id);
+        $article->title = $_POST['title'];
+        $article->text = $_POST['text'];
+        $article->save();
+        header('Location: /');
+        exit;
+}
+include __DIR__ . '/../Views/oneNews.php';
