d5493f4aca719980833a4f2b4e5e095d057a17b0
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-06 20:36:43 +0500

    + __set(), __get(), display()

--- App/View.php
+++ App/View.php
@@ -6,6 +6,20 @@ namespace App;
 class View
 {
 
+    protected $data = [];
 
+    public function __set($k, $v)
+    {
+        $this->data[$k] = $v;
+    }
 
+    public function __get($k)
+    {
+        return $this->data[$k];
+    }
+
+    public function display($template)
+    {
+        include $template;
+    }
 }
