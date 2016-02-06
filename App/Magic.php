ff33b841c295575127137427c073e6bb121a7d62
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-06 22:25:23 +0500

    вынес магию в трейт

---
+++ App/Magic.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+
+trait Magic
+{
+    protected $data = [];
+
+    public function __set($k, $v)
+    {
+        $this->data[$k] = $v;
+    }
+
+    public function __get($k)
+    {
+        return $this->data[$k];
+    }
+
+    public function __isset($k)
+    {
+        return isset($this->data[$k]);
+    }
+
+    public function __unset($k)
+    {
+        unset($this->data[$k]);
+    }
+}
--- App/View.php
+++ App/View.php
@@ -6,27 +6,7 @@ namespace App;
 class View
 {
 
-    protected $data = [];
-
-    public function __set($k, $v)
-    {
-        $this->data[$k] = $v;
-    }
-
-    public function __get($k)
-    {
-        return $this->data[$k];
-    }
-
-    public function __isset($k)
-    {
-        return isset($this->data[$k]);
-    }
-
-    public function __unset($k)
-    {
-        unset($this->data[$k]);
-    }
+    use Magic;
 
     public function display($template)
     {
