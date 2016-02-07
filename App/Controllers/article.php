99b77b0164d8659965f6bff16c7400af2abd2573
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-07 22:00:22 +0500

    изменил контроллер вывода одной новости

--- App/Controllers/article.php
+++ App/Controllers/article.php
@@ -1,13 +1,15 @@
 <?php
 
 use \App\Models\News;
+use \App\View;
 
 require __DIR__ . '/../../autoload.php';
 
 if(!empty($_GET['id'])) {
     $id = $_GET['id'];
-    $article = News::findById($id);
-    include __DIR__ . '/../views/news/oneNews.php';
+    $view = new View();
+    $view->article = News::findById($id);
+    $view->display(__DIR__ . '/../views/news/oneNews.php');
 } else {
     header('Location: /');
     exit;
