f91957157553e98a9b667d86c784a1c89613e403
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-07 21:25:42 +0500

    изменил контроллер вывода всех новостей

--- App/Controllers/allNews.php
+++ App/Controllers/allNews.php
@@ -1,9 +1,10 @@
 <?php
 
+use \App\View;
 use \App\Models\News;
 
 require __DIR__ . '/../../autoload.php';
 
-$allNews = News::findAllDesc();
-
-include __DIR__ . '/../views/news/allNews.php';
+$view = new View();
+$view->news = News::findAllDesc();
+$view->display(__DIR__ . '/../views/news/allNews.php');
