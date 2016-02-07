ef6e95cef40d9aa2c58a44642ba66dbb535afdc1
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-07 22:09:07 +0500

    изменил шаблон вывода одной новости

--- App/views/news/oneNews.php
+++ App/views/news/oneNews.php
@@ -1,26 +1,53 @@
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title><?php echo $article->title ?></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title><?php echo $article->title; ?></title>
+
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-    <header>
-        <h2><?php echo $article->title; ?></h2>
-    </header>
-    <article>
-        <?php echo $article->text; ?>
-    </article>
-    <hr>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <p>
+                <h3><?php echo $article->title; ?></h3>
+            </p>
+        </div>
+        <div class="panel-body">
+            <p>
+                <?php echo $article->text; ?>
+            </p>
+        </div>
+        <div class="panel-footer">
+            <p>
+                Автор: <?php echo $article->author->name; ?>
+            </p>
+        </div>
+    </div>
     <p>
         <a href="/App/Controllers/admin.php?id=<?php echo $article->id; ?>">
-            <button>Редактировать новость</button>
+            <button type="button" class="btn btn-primary btn-md">Редактировать новость</button>
         </a>
     </p>
     <p>
         <a href="/index.php">
-            <button>На главную</button>
+            <button type="button" class="btn btn-primary btn-md">На главную</button>
         </a>
     </p>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
