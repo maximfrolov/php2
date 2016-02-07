feb3a63bdaccaa2233c66ab751c5cadbefbe5e61
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-08 02:27:29 +0500

    изменил контроллер формы добавления новости

--- App/Controllers/add.php
+++ App/Controllers/add.php
@@ -1,16 +1,21 @@
 <?php
 
 use \App\Models\News;
+use \App\View;
 
 require __DIR__ . '/../../autoload.php';
 
-if (!empty($_POST['title']) && !empty($_POST['text'])) {
-    $article = new News();
-    $article->title = $_POST['title'];
-    $article->text = $_POST['text'];
-    $article->save();
+$view = new View();
+if (!empty($_POST['title']) &&
+    !empty($_POST['text']) &&
+    !empty($_POST['author_id'])) {
+    $view->news = new News();
+    $view->news->title     = $_POST['title'];
+    $view->news->text      = $_POST['text'];
+    $view->news->author_id = $_POST['author_id'];
+    $view->news->save();
     header('Location: /');
     exit;
 } else {
-    include __DIR__ . '/../views/news/add.php';
+    $view->display(__DIR__ . '/../views/news/add.php');
 }
