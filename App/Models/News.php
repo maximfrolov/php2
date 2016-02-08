6f71c091895eeb9c05dbc8758edfcd20d5c8c3c6
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-08 16:25:01 +0500

    + PHPDoc

--- App/Models/News.php
+++ App/Models/News.php
@@ -5,16 +5,32 @@ namespace App\Models;
 use App\Model;
 use App\Db;
 
+/**
+ * Class News Модель новостей
+ * @package App\Models
+ */
 class News
     extends Model
 {
 
+    /**
+     * Константа класса - имя таблицы в БД
+     */
     const TABLE = 'news';
 
+    /**
+     * @property $title string Заголовок новости
+     * @property $text string Текст новости
+     * @property $author_id integer id автора новости
+     */
     public $title;
     public $text;
     public $author_id;
 
+    /**
+     * Метод нахождения последних 3-х новостей
+     * @return array Массив объектов
+     */
     public static function lastNews()
     {
         $db = Db::instance();
@@ -25,6 +41,11 @@ class News
         );
     }
 
+    /**
+     * Метод нахождения всех новостей и сортировки их
+     * в обратном порядке, по добавлению в БД
+     * @return array Массив объектов
+     */
     public static function findAllDesc()
     {
         $db = Db::instance();
@@ -36,6 +57,13 @@ class News
 
     }
 
+    /**
+     * Магический метод __get();
+     * проверяет на пустоту поле author_id,
+     * при запросе поля author
+     * @param $k string Имя недоступного свойства
+     * @return bool|object Вернет объект класса Author или false
+     */
     public function __get($k)
     {
         if ('author' == $k && !empty($this->author_id)) {
