3686bf55c30eab061b222f2e4cf434135bdc233c
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-08 14:15:49 +0500

    изменил контроллер редактирования новости

--- App/Controllers/update.php
+++ App/Controllers/update.php
@@ -1,18 +1,22 @@
 <?php
 
 use App\Models\News;
+use App\View;
 
 require __DIR__ . '/../../autoload.php';
 
+$view = new View();
 if (!empty($_POST['title']) &&
     !empty($_POST['text']) &&
+    !empty($_POST['author_id']) &&
     !empty($_POST['id'])) {
-        $id = $_POST['id'];
-        $article = News::findById($id);
-        $article->title = $_POST['title'];
-        $article->text = $_POST['text'];
-        $article->save();
-        header('Location: /');
-        exit;
+    $id = $_POST['id'];
+    $view->article = News::findById($id);
+    $view->article->title     = $_POST['title'];
+    $view->article->text      = $_POST['text'];
+    $view->article->author_id = $_POST['author_id'];
+    $view->article->save();
+    header('Location: /');
+    exit;
 }
-include __DIR__ . '/../views/news/admin.php';
+$view->display(__DIR__ . '/../views/news/admin.php');
