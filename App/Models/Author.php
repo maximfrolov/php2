af2dd63cc3e1d1f5487562eda766de93c3f8c76f
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-08 16:28:17 +0500

    + PHPDoc

--- App/Models/Author.php
+++ App/Models/Author.php
@@ -5,12 +5,22 @@ namespace App\Models;
 
 use App\Model;
 
+/**
+ * Class Author
+ * @package App\Models
+ */
 class Author
     extends Model
 {
 
+    /**
+     * Константа класса - имя таблицы в БД
+     */
     const TABLE = 'authors';
 
+    /**
+     * @property string Имя автора новости
+     */
     public $name;
 
 }
