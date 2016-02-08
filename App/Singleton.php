6be02e136ecf609834a2a01e5a023fb3963774c4
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-08 18:04:38 +0500

    + PHPDoc

--- App/Singleton.php
+++ App/Singleton.php
@@ -6,12 +6,24 @@ namespace App;
 trait Singleton
 {
 
+    /**
+     * @var object Содержит в себе единственный экземпляр класса
+     */
     protected static $instance;
 
+    /**
+     *  Защищенный конструктор
+     *  Не дает автоматически создать объект класса
+     */
     protected function __construct()
     {
     }
 
+    /**
+     * Метод, создающий или возвращающий
+     * единственный экземпляр класса
+     * @return object 
+     */
     public static function instance()
     {
         if (null === static::$instance) {
