8a2e851fa7c7c433f14710a84d4e59f763a38bde
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-09 19:13:36 +0500

    изменил контроллер удаления новости

--- App/Controllers/delete.php
+++ App/Controllers/delete.php
@@ -1,15 +1,17 @@
 <?php
 
 use \App\Models\News;
+use \App\View;
 
 require __DIR__ . '/../../autoload.php';
 
+$view = new View();
 if (!empty($_POST['id'])) {
     $id = $_POST['id'];
-    $article = News::findById($id);
-    $article->delete();
+    $view->article = News::findById($id);
+    $view->article->delete();
     header('Location: /');
     exit;
 } else {
-    include __DIR__ . '/../views/news/admin.php';
+    $view->display(__DIR__ . '/../views/news/admin.php');
 }
