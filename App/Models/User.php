bd04f0710816d8a5ab005dcc51773bb1776d5252
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-09 20:25:48 +0500

    + PHPDoc

--- App/Models/User.php
+++ App/Models/User.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use App\Model;
 
+/**
+ * Class User Модель пользователей
+ * @package App\Models
+ */
 class User
     extends Model
 {
+    /**
+     * Константа класса - имя таблицы в БД
+     */
     const TABLE = 'users';
 
+    /**
+     * @property $name string Имя пользователя
+     * @property $email string e-mail пользователя
+     */
     public $name;
     public $email;
 
