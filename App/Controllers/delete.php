f84f131e1f017f60bc4adba56f323628fe189568
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-11 03:14:32 +0500

    - вся работа с объектом View();

--- App/Controllers/delete.php
+++ App/Controllers/delete.php
@@ -1,17 +1,8 @@
 <?php
 
-use \App\Models\News;
-use \App\View;
+use \App\Controllers\News;
 
 require __DIR__ . '/../../autoload.php';
 
-$view = new View();
-if (!empty($_POST['id'])) {
-    $id = $_POST['id'];
-    $view->article = News::findById($id);
-    $view->article->delete();
-    header('Location: /');
-    exit;
-} else {
-    $view->display(__DIR__ . '/../views/news/admin.php');
-}
+$controller = new News();
+$controller->action('Delete');
