09c6e0e87eeed26ddc725411930bfe8470e2f6b4
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-11 02:13:35 +0500

    - вся работа с оьъектом View();

--- App/Controllers/add.php
+++ App/Controllers/add.php
@@ -1,21 +1,8 @@
 <?php
 
-use \App\Models\News;
-use \App\View;
+use \App\Controllers\News;
 
 require __DIR__ . '/../../autoload.php';
 
-$view = new View();
-if (!empty($_POST['title']) &&
-    !empty($_POST['text']) &&
-    !empty($_POST['author_id'])) {
-    $view->news = new News();
-    $view->news->title     = $_POST['title'];
-    $view->news->text      = $_POST['text'];
-    $view->news->author_id = $_POST['author_id'];
-    $view->news->save();
-    header('Location: /');
-    exit;
-} else {
-    $view->display(__DIR__ . '/../views/news/add.php');
-}
+$controller = new News();
+$controller->action('Add');
