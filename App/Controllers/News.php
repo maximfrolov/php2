aab451c22822aef0aa30edac1b84cc2465d6dd9c
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-10 20:34:13 +0500

    + методы action($action); и before();

--- App/Controllers/News.php
+++ App/Controllers/News.php
@@ -12,7 +12,8 @@ class News
 {
 
     /**
-     * @property View object Защищенное свойство хранит в себе объект View();
+     * @property View object Защищенное свойство
+     * хранит в себе объект View();
      */
     protected $view;
 
@@ -20,7 +21,7 @@ class News
      * News constructor.
      * При создании объекта App\Controllers\News,
      * автоматически создается объект View()
-     * и сохраняется в свойство $view
+     * и сохраняется в свойство $view.
      */
     public function __construct()
     {
@@ -28,9 +29,33 @@ class News
     }
 
     /**
-     * Метод-экшн, для вывода последних новостей
+     * Proxy method
+     * Метод, получающий имя экшна,
+     * выполняющий какие-то действия перед экшном,
+     * выполняющий сам экшн.
+     * @param $action
+     * @return mixed
      */
-    public function actionIndex()
+    public function action($action)
+    {
+        $methodName = 'action' . $action;
+        $this->beforeAction();
+        return $this->$methodName();
+    }
+
+    /**
+     * Метод, который будет вызываться
+     * автоматически перед любым экшном.
+     */
+    protected function beforeAction()
+    {
+        //echo 'счетчик';
+    }
+
+    /**
+     * Метод-экшн, для вывода последних новостей.
+     */
+    protected function actionIndex()
     {
         $this->view->news = \App\Models\News::lastNews();
         $this->view->display(__DIR__ . '/../views/news/lastNews.php');
--- index.php
+++ index.php
@@ -5,4 +5,4 @@ use \App\Controllers\News;
 require __DIR__ . '/autoload.php';
 
 $controller = new News();
-$controller->actionIndex();
+$controller->action('Index');
