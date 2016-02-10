05ce10d8c5598886526610ad274d1224cf38d78f
Maxim Frolov <frolov-max74@yandex.ru>
2016-02-11 02:48:59 +0500

    - вся работа с объектом View();

--- App/Controllers/update.php
+++ App/Controllers/update.php
@@ -1,22 +1,8 @@
 <?php
 
-use App\Models\News;
-use App\View;
+use \App\Controllers\News;
 
 require __DIR__ . '/../../autoload.php';
 
-$view = new View();
-if (!empty($_POST['title']) &&
-    !empty($_POST['text']) &&
-    !empty($_POST['author_id']) &&
-    !empty($_POST['id'])) {
-    $id = $_POST['id'];
-    $view->article = News::findById($id);
-    $view->article->title     = $_POST['title'];
-    $view->article->text      = $_POST['text'];
-    $view->article->author_id = $_POST['author_id'];
-    $view->article->save();
-    header('Location: /');
-    exit;
-}
-$view->display(__DIR__ . '/../views/news/admin.php');
+$controller = new News();
+$controller->action('Update');
