6738863dc35b27f043397a7e05e1a2664c896b56
Maxim Frolov <consumer2@mail.ru>
2016-02-19 11:15:45 +0500

    редактировал шаблон

--- App/views/admin/edit.php
+++ App/views/admin/edit.php
@@ -1,52 +1,61 @@
 <!DOCTYPE html>
-<html lang="ru"
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Админ-панель</title>
-    <style>
-        fieldset {
-            width: 50%;
-        }
-    </style>
+
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-<div>
+<div class="container">
     <h2>Редактирование новости:</h2>
+    <form method="post" action="/admin/admin/update/">
+        <input type="hidden" name="id" value="<?php echo $article->id ?>">
+        <br>
+        <label for="title">Заголовок:</label>
+        <br>
+        <input type="text" name="title" value="<?php echo $article->title; ?>">
+        <br>
+        <label for="text">Текст:</label>
+        <br>
+        <textarea cols="50" rows="5" name="text"><?php echo $article->text; ?></textarea>
+        <br>
+        <label for="author_id">ID автора:</label>
+        <br>
+        <input type="number" name="author_id" value="<?php echo $article->author_id ?>">
+        <p>
+            <button type="submit">Обновить новость</button>
+            <button type="reset">Сброс</button>
+        </p>
+    </form>
+    <!-- форма для удаления новости -->
+    <form method="post" action="/admin/admin/delete/">
+        <input type="hidden" name="id" value="<?php echo $article->id ?>">
+        <p>
+            <button type="submit">Удалить новость</button>
+        </p>
+    </form>
+    <p>
+        <a href="/admin/admin/index/">
+            <button>Назад</button>
+        </a>
+    </p>
 </div>
-    <fieldset>
-        <legend>Редактирование новости:</legend>
-        <form method="post" action="/admin/admin/update/">
-            <input type="hidden" name="id" value="<?php echo $article->id ?>">
-            <br>
-            <label for="title">Заголовок:</label>
-            <br>
-            <input type="text" name="title" value="<?php echo $article->title; ?>">
-            <br>
-            <label for="text">Текст:</label>
-            <br>
-            <textarea cols="50" rows="5" name="text"><?php echo $article->text; ?></textarea>
-            <br>
-            <label for="author_id">ID автора:</label>
-            <br>
-            <input type="number" name="author_id" value="<?php echo $article->author_id ?>">
-            <p>
-                <button type="submit">Обновить новость</button>
-                <button type="reset">Сброс</button>
-            </p>
-        </form>
-        <!-- форма для удаления новости -->
-        <form method="post" action="/admin/admin/delete/">
-            <input type="hidden" name="id" value="<?php echo $article->id ?>">
-            <p>
-                <button type="submit">Удалить новость</button>
-            </p>
-        </form>
-    </fieldset>
-</div>
-<p>
-    <a href="/admin/admin/index/">
-        <button>Назад</button>
-    </a>
-</p>
+
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
 </body>
 </html>
