c217b8318944c68c375b64c93c7d269a207d031e
Maxim Frolov <consumer2@mail.ru>
2016-02-26 21:22:48 +0500

    изменил шаблон вывода ошибок

--- App/views/errors/error.php
+++ App/views/errors/error.php
@@ -33,8 +33,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="/index.php">Все новости</a></li>
-                <!--<li><a href="/admin/admin/add/">Добавить новость</a></li>-->
+                <li><a href="/news/index/">На главную</a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
